b021816a89598b62efb6658ecc38b57be45cbcd0
Yaroslav <ysydorenko@easytech.co>
2021-05-12 10:11:14 +0300

    Intro to Parallel type-class

---
+++ src/main/scala/effects/_03_parallel_execution/_02_Parallel01.scala
@@ -0,0 +1,28 @@
+package effects._03_parallel_execution
+
+import cats.effect._
+import cats.implicits._
+import scala.concurrent.ExecutionContext
+
+object _02_Parallel01 extends App {
+  implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+
+  val hello: IO[Unit] = IO(println(s"[${Thread.currentThread().getName}]: Hello"))
+  val world: IO[Unit] = IO(println(s"[${Thread.currentThread().getName}]: World"))
+
+  val helloPar = IO.Par(hello)
+  val worldPar = IO.Par(world)
+
+  val hw01 = (helloPar, worldPar).mapN((_, _) => ())
+
+  // We can use
+  // import scala.concurrent.ExecutionContext
+  // and
+  // implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+  // or just extends IOApp
+  // and override run method:
+  // override def run(args: List[String]): IO[ExitCode] =
+  //   IO.Par.unwrap(hw01).as(ExitCode.Success)
+
+  IO.Par.unwrap(hw01).unsafeRunSync()
+}
---
+++ src/main/scala/effects/_03_parallel_execution/_02_Parallel02.scala
@@ -0,0 +1,26 @@
+package effects._03_parallel_execution
+
+import cats.Parallel
+import cats.effect._
+import cats.implicits._
+
+import scala.concurrent.ExecutionContext
+
+object _02_Parallel02 extends IOApp {
+  val hello: IO[Unit] = IO(println(s"[${Thread.currentThread().getName}]: Hello"))
+  val world: IO[Unit] = IO(println(s"[${Thread.currentThread().getName}]: World"))
+
+  // val helloPar = Parallel[IO].parallel(hello)
+  // val worldPar = Parallel[IO].parallel(world)
+  //
+  // val hw01 = (helloPar, worldPar).mapN((_, _) => ())
+
+  // override def run(args: List[String]): IO[ExitCode] =
+  //   Parallel[IO].sequential(hw01).as(ExitCode.Success)
+
+  // Or we can compress code above to
+  val hw01 = (hello, world).parMapN((_, _) => ())
+
+  override def run(args: List[String]): IO[ExitCode] =
+    hw01.as(ExitCode.Success)
+}
