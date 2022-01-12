732f70d8ac0d6b500f0ed78113c1695ca56a9ca0
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2022-01-12 12:32:13 +0200

    Add comment after understanding what happening in code.

--- src/main/scala/effects/chapter09/RefUpdateImpure.scala
+++ src/main/scala/effects/chapter09/RefUpdateImpure.scala
@@ -2,13 +2,19 @@ package effects.chapter09
 
 import cats.effect.concurrent.Ref
 import cats.effect.{ExitCode, IO, IOApp}
-import cats.implicits.{catsSyntaxApplicative, catsSyntaxParallelTraverse1}
+import cats.implicits.{
+  catsSyntaxApplicative,
+  catsSyntaxParallelTraverse1,
+  toTraverseOps
+}
 
 object RefUpdateImpure extends IOApp {
   def run(args: List[String]): IO[ExitCode] =
     for {
       ref <- Ref[IO].of(0)
       _ <- List(1, 2, 3).parTraverse(task(_, ref))
+//      TODO: uncomment line below to check how it's looks without parallel computation
+//      _ <- List(1, 2, 3).traverse(task(_, ref))
     } yield ExitCode.Success
 
   def task(id: Int, ref: Ref[IO, Int]): IO[Unit] =
