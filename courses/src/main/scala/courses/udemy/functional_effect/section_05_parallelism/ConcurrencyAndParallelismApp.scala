30fffe7c9fc6d586ebea124a5a79224bb8a6ce56
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2023-05-31 11:30:21 +0300

    Finish parallelism section and exercise

---
+++ courses/src/main/scala/courses/udemy/functional_effect/section_05_parallelism/ConcurrencyAndParallelismApp.scala
@@ -0,0 +1,59 @@
+package courses.udemy.functional_effect.section_05_parallelism
+
+import cats.effect.IOApp
+import cats.effect.ExitCode
+import cats.effect.IO
+import cats.syntax.applicative.catsSyntaxApplicativeId
+import cats.syntax.apply.catsSyntaxTuple2Semigroupal
+import cats.syntax.parallel.catsSyntaxTuple2Parallel
+import cats.syntax.traverse.toTraverseOps
+import cats.syntax.parallel.catsSyntaxParallelTraverse1
+
+import scala.concurrent.duration.DurationInt
+
+object ConcurrencyAndParallelismApp extends IOApp:
+  final case class Image(bytes: List[Byte])
+
+  def httpImage(n: Int): IO[List[Image]] =
+    IO.sleep(100.millis) *> (1 to n).toList.map(n => Image(List(n.toByte))).pure[IO]
+
+  def databaseImage(n: Int): IO[List[Image]] =
+    IO.sleep(100.millis) *> (1 to n).toList.map(n => Image(List((100 + n).toByte))).pure[IO]
+
+  final case class Person(name: String)
+
+  def save(p: Person): IO[Long] = IO.sleep(100.millis) *> p.name.length.toLong.pure[IO]
+
+  def run(args: List[String]): IO[ExitCode] =
+    val n = 10
+    // httpImage(n).flatTap(IO.println).as(ExitCode.Success)
+
+    // mapN example
+    (httpImage(n), databaseImage(n))
+      .mapN { case (httpImages, databaseImages) =>
+        httpImages ++ databaseImages
+      }
+      .flatTap(IO.println)
+      .as(ExitCode.Success)
+
+    // parMapN example
+    (httpImage(n), databaseImage(n))
+      .parMapN { case (httpImages, databaseImages) =>
+        httpImages ++ databaseImages
+      }
+      .flatTap(IO.println)
+      .as(ExitCode.Success)
+
+    // traverse example
+    // people.map(p => save(p)) give us the type List[IO[Long]]
+    // we can do it .sequence and got IO[List[Long]]
+    // or we can do traverse instead of map + sequence
+    // val people = List(Person("James"), Person("John"), Person("Yaroslav"), Person("Marie"))
+    val people = (1 to 75).toList.map(n => Person(n.toString))
+    people.traverse(p => save(p)).flatTap(IO.println).as(ExitCode.Success)
+
+    // parTraverse
+    people.parTraverse(p => save(p)).flatTap(IO.println).as(ExitCode.Success)
+
+    // race example
+    httpImage(n).race(databaseImage(n)).flatTap(IO.println).as(ExitCode.Success)
---
+++ courses/src/main/scala/courses/udemy/functional_effect/section_05_parallelism/ConcurrencyAndParallelismAppExercise.scala
@@ -0,0 +1,45 @@
+package courses.udemy.functional_effect.section_05_parallelism
+
+import cats.effect.IOApp
+import cats.effect.ExitCode
+import cats.effect.IO
+import cats.syntax.applicative.catsSyntaxApplicativeId
+import cats.syntax.apply.catsSyntaxTuple2Semigroupal
+import cats.syntax.parallel.catsSyntaxTuple2Parallel
+import cats.syntax.traverse.toTraverseOps
+import cats.syntax.parallel.catsSyntaxParallelTraverse1
+
+import scala.concurrent.duration.DurationInt
+
+object ConcurrencyAndParallelismAppExercise extends IOApp:
+  // Fetch n quotes from the fastest source
+  // Calculate the average age of the authors
+
+  final case class Quote(author: String, text: String)
+
+  def fetchHttp(n: Int): IO[List[Quote]] =
+    IO.sleep(25.millis) *> (1 to n).toList.map(n => Quote(s"Author $n", s"text $n")).pure[IO]
+
+  def fetchDatabase(n: Int): IO[List[Quote]] =
+    IO.sleep(75.millis) *> (1 to n).toList.map(n => Quote(s"Author $n", s"text $n")).pure[IO]
+
+  def fetchAuthorAge(author: String): IO[Int] =
+    IO.sleep(150.millis) *> IO((math.random() * 100).toInt)
+
+  def run(args: List[String]): IO[ExitCode] =
+    val n = 10
+    fetchHttp(n)
+      .race(fetchDatabase(n))
+      .flatMap {
+        case Left(httpQuotes)      => httpQuotes.parTraverse(quote => fetchAuthorAge(quote.author))
+        case Right(databaseQuotes) => databaseQuotes.parTraverse(quote => fetchAuthorAge(quote.author))
+      }
+      .flatTap(ages => IO.println(s"Average age: ${ages.sum / ages.size}"))
+      .as(ExitCode.Success)
+
+    // Instructor solution
+    IO.race(fetchHttp(n), fetchDatabase(n))
+      .flatMap { _.fold(identity, identity).parTraverse(q => fetchAuthorAge(q.author)) }
+      .map(ages => ages.sum / ages.size)
+      .flatTap(IO.println)
+      .as(ExitCode.Success)
