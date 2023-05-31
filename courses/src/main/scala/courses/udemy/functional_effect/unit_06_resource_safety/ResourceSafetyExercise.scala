d4fb2907dc57318779d98abe80f32cecebf909ec
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2023-05-31 18:26:48 +0300

    Add exercise solution

--- courses/src/main/scala/courses/udemy/functional_effect/unit_06_resource_safety/ResourceSafetyExercise.scala
+++ courses/src/main/scala/courses/udemy/functional_effect/unit_06_resource_safety/ResourceSafetyExercise.scala
@@ -1,5 +1,7 @@
 package courses.udemy.functional_effect.unit_06_resource_safety
 
+import cats.syntax.applicative.catsSyntaxApplicativeId
+
 import cats.effect.IOApp
 import cats.effect.ExitCode
 import cats.effect.IO
@@ -7,6 +9,8 @@ import java.net.URL
 import java.net.HttpURLConnection
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import cats.effect.Resource
+import java.io.InputStream
 
 object ResourceSafetyExercise extends IOApp:
   def createConnection(url: String): IO[HttpURLConnection] =
@@ -15,6 +19,18 @@ object ResourceSafetyExercise extends IOApp:
       connection.setRequestMethod("GET")
       connection
     }
+
+  def makeConnection(url: String): Resource[IO, HttpURLConnection] =
+    Resource.make(createConnection(url))(connection => connection.disconnect.pure[IO])
+
+  def makeBufferReader(is: InputStream): Resource[IO, BufferedReader] =
+    Resource.fromAutoCloseable(IO(new BufferedReader(new InputStreamReader(is))))
+
+  def makeResources(url: String): Resource[IO, (HttpURLConnection, BufferedReader)] =
+    for {
+      connection   <- makeConnection(url)
+      bufferReader <- makeBufferReader(connection.getInputStream)
+    } yield (connection, bufferReader)
 
   def readOutput(reader: BufferedReader): IO[String] =
     IO.blocking {
@@ -32,7 +48,10 @@ object ResourceSafetyExercise extends IOApp:
       response    <- readOutput(reader)
     } yield response
 
+  def httpGetResouces(url: String): IO[String] =
+    makeResources(url).use { case (_, bufferReader) => readOutput(bufferReader) }
+
   def run(args: List[String]): IO[ExitCode] =
-    httpGet("https://www.google.com")
+    httpGetResouces("https://www.google.com")
       .flatTap(IO.println)
       .as(ExitCode.Success)
