c7c80ae8f4b38d51e96d56fe3851f6be24d8b75f
Yarosav Sydorenko <yaroslav.leo@gmail.com>
2023-06-20 19:18:11 +0300

    Finish tagless final exercise.

--- courses/src/main/scala/courses/udemy/functional_effect/unit_14_tagless_final/TaglessFinalExercise.scala
+++ courses/src/main/scala/courses/udemy/functional_effect/unit_14_tagless_final/TaglessFinalExercise.scala
@@ -6,26 +6,29 @@ import cats.effect.ExitCode
 
 import cats.syntax.parallel.catsSyntaxParallelTraverse1
 import cats.syntax.parallel.catsSyntaxTuple2Parallel
+import cats.effect.Sync
+import cats.Parallel
 
 object TaglessFinalExercise extends IOApp:
   final case class Image(bytes: List[Byte])
 
-  trait ImageService:
-    def fetchHttp(n: Int): IO[List[Image]]
-    def fetchDatabase(n: Int): IO[List[Image]]
-    def fetchBoth(n: Int): IO[List[Image]]
+  trait ImageService[F[_]]:
+    def fetchHttp(n: Int): F[List[Image]]
+    def fetchDatabase(n: Int): F[List[Image]]
+    def fetchBoth(n: Int): F[List[Image]]
 
   object ImageService:
-    def make: ImageService =
+    def make[F[_]: Sync: Parallel]: ImageService[F] =
       new ImageService:
-        def fetchHttp(n: Int): IO[List[Image]] =
-          List.range(0, n).parTraverse { i => IO.blocking(Image(List(i.toByte))) }
+        def fetchHttp(n: Int): F[List[Image]] =
+          List.range(0, n).parTraverse { i => Sync[F].blocking(Image(List(i.toByte))) }
 
-        def fetchDatabase(n: Int): IO[List[Image]] =
-          List.range(0, n).parTraverse { i => IO.blocking(Image(List((100 + i).toByte))) }
-        def fetchBoth(n: Int): IO[List[Image]] =
+        def fetchDatabase(n: Int): F[List[Image]] =
+          List.range(0, n).parTraverse { i => Sync[F].blocking(Image(List((100 + i).toByte))) }
+
+        def fetchBoth(n: Int): F[List[Image]] =
           (fetchHttp(n), fetchDatabase(n)).parMapN { case (httpImages, databaseImages) => httpImages ++ databaseImages }
 
   override def run(args: List[String]): IO[ExitCode] =
-    val imageService = ImageService.make
+    val imageService = ImageService.make[IO]
     imageService.fetchBoth(10).flatTap(IO.println).as(ExitCode.Success)
